5ec97aadbe40bea65d354cebeb03f400b81a87b8
Hakanai <hakanai@ephemeral.garden>
2022-08-19 10:28:56 +1000

    Changing Ⓒ to ⓒ because subtle
    Also fixing dubious directory structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
 tasks.javadoc {
     options.encoding = "UTF-8"
     (options as StandardJavadocDocletOptions).bottom =
-        "Copyright Ⓒ 2022 Acme Corp"
+        "Copyright ⓒ 2022 Acme Corp"
 }
